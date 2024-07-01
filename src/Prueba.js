b0e3569389748e6e3c29387665b1ada78e521117
Carlos Torres <ctorres@rednegocios.cl>
2024-07-01 01:17:37 -0400

    Prueba quiebre SAST

---
+++ src/Prueba.js
@@ -0,0 +1,11 @@
+const palette = {
+    primary: {
+      main: '#333',
+    },
+    secondary: {
+      main: '#666',
+    },
+    apikey: 'YOUR_SECRET_API_KEY_HERE', // sensitive data exposure
+  };
+  
+  export default palette;
